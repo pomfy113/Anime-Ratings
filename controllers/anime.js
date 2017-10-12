0d780e2d2bfcff99be89b9ee8463a6cf6d1db1ff
pomfy113 <pomfy113@gmail.com>
2017-10-12 12:28:39 -0700

    Minor fixes before working on styling

--- controllers/anime.js
+++ controllers/anime.js
@@ -25,16 +25,13 @@ module.exports = function(app) {
             comment = acomment
         });
         kitsuanime.getAnime(req.params.anime_id)
-            .then(results =>
-                {res.render('anime-show', {anime: results, comment: comment})
-                console.log("BLA BLA console.log thing", comment, "\n\n")})
+            .then(results => res.render('anime-show', {anime: results, comment: comment}))
             .catch(err => console.error(err));
     })
 
     //CREATE; first comment for an anime?
     app.post('/anime/:id/', function (req, res) {
         AnimeComment.create(req.body, function(err, acomment) {
-            console.log(acomment)
             res.redirect('/anime/' + req.params.id);
       })
   })
@@ -44,11 +41,4 @@ module.exports = function(app) {
         })
       })
 
-  //   app.post('/anime/:id/', function (req, res) {
-  //   AnimeComment.find( { kitsuId: req.params.id },  req.body, function(err, acomment) {
-  //       acomment.comments.push(req.body);
-  //       acomment.save()
-  //       res.redirect('/anime/' + acomment.kitsuId);
-  //   })
-  // })
 }
