999f2c28801d040942ab01538053208dfbeeb9c7
pomfy113 <pomfy113@gmail.com>
2017-10-13 11:33:01 -0700

    Pre-installation of MAL api

--- controllers/anime.js
+++ controllers/anime.js
@@ -1,8 +1,10 @@
+// Kitsu is the API
 var Kitsu = require('kitsu.js');
 var kitsuanime = new Kitsu();
+
 var mongoose = require('mongoose');
 
-
+//Move this to seperate model later
 var AnimeComment = mongoose.model('AnimeComment', {
     comment: String,
     rating: Number,
@@ -12,13 +14,14 @@ var AnimeComment = mongoose.model('AnimeComment', {
 
 module.exports = function(app) {
 
+    // Search function
     app.get('/search', function (req, res) {
         kitsuanime.searchAnime(req.query.term)
             .then(results => res.render('anime-search', {results: results}))
             .catch(err => console.error(err));
     })
 
-    //
+    // Grab comments, then anime
     app.get('/anime/:anime_id', function (req, res) {
         var comment;
         AnimeComment.find({ kitsuId : req.params.anime_id }, function(err, acomment){
@@ -29,12 +32,14 @@ module.exports = function(app) {
             .catch(err => console.error(err));
     })
 
-    //CREATE; first comment for an anime?
+    //CREATE; comment for an anime
     app.post('/anime/:id/', function (req, res) {
         AnimeComment.create(req.body, function(err, acomment) {
             res.redirect('/anime/' + req.params.id);
       })
   })
+
+    //DELETE; comment for anime
       app.delete('/anime/:id/:comment_id', function (req, res) {
         AnimeComment.findByIdAndRemove(req.params.comment_id, function(err) {
           res.redirect('/anime/' + req.params.id);
---
+++ readme.md
@@ -0,0 +1,3 @@
+## Hello there!
+- You can safely ignore anything show related! It all should work with anything listed as "anime"
+- I basically combined the search engine and the Rotten Potatoes creation bit.
