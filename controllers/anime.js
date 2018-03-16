0709581858266284a474492eabec09cd08afd08e
pomfy113 <pomfy113@gmail.com>
2018-03-15 18:16:37 -0700

    Just doing something for other project

--- controllers/anime.js
+++ controllers/anime.js
@@ -154,7 +154,44 @@ module.exports = function(app) {
         const date = new Date();
         const year = date.getFullYear();
         const seasonList = ["winter", "spring", "summer", "fall"];
-        const seasonCount = Math.floor(date.getMonth() / 3)
+        const seasonCount = Math.floor(date.getMonth() / 3);
+
+        const season = seasonList[seasonCount];
+
+        malScraper.getSeason(year, season)
+          .then((data) => {
+              let animeTV = [];
+              let biggerpic;
+              // Do some minor altering for the data
+              for(let item in data.TV){
+                  biggerpic = data.TV[item].picture.replace('r/167x242/', '');
+                  data.TV[item].picture = biggerpic;
+
+                  if(data.TV[item].score === "N/A"){
+                      data.TV[item].score = "0"; // I'll need to revert this later
+                  }
+
+                  animeTV.push(data.TV[item]);
+              }
+
+              const sorted = animeTV.sort(function(a, b){
+                  return b.score - a.score;
+              });
+
+              res.render('home/alt-home', {
+                  MAL_TV: JSON.stringify(animeTV),
+                  season: JSON.stringify({year: year, season: seasonCount})
+              });
+          })
+
+          .catch((err) => console.log(err));
+    });
+
+    app.get('/', (req, res) => {
+        const date = new Date();
+        const year = date.getFullYear();
+        const seasonList = ["winter", "spring", "summer", "fall"];
+        const seasonCount = Math.floor(date.getMonth() / 3);
 
         const season = seasonList[seasonCount];
 
@@ -201,7 +238,37 @@ module.exports = function(app) {
                   if(cleanedData[item].score === "N/A"){
                      cleanedData[item].score = "0"; // I'll need to revert this later
                   }
+              }
 
+             return cleanedData.sort(function(a, b){
+                  return b.score - a.score;
+              })
+          }).then((data) => {
+              res.send(JSON.stringify(data));
+          }).catch((err) => console.log(err));
+    });
+
+    app.get('/get-current', (req, res) => {
+        const date = new Date();
+        const year = date.getFullYear();
+        const seasonList = ["winter", "spring", "summer", "fall"];
+        const seasonCount = Math.floor(date.getMonth() / 3);
+
+        const season = seasonList[seasonCount];
+
+        malScraper.getSeason(year, season)
+          .then((data) => {
+              let cleanedData = data.TV
+              let biggerpic;
+
+              // Do some minor altering for the data
+              for(let item in cleanedData){
+                  biggerpic = cleanedData[item].picture.replace('r/167x242/', '');
+                  cleanedData[item].picture = biggerpic;
+
+                  if(cleanedData[item].score === "N/A"){
+                     cleanedData[item].score = "0"; // I'll need to revert this later
+                  }
               }
 
              return cleanedData.sort(function(a, b){
