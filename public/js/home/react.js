a0e40671dade583166920d533641aa585c350c79
pomfy113 <pomfy113@gmail.com>
2018-03-20 13:19:12 -0700

    Cleaned up components

--- public/js/home/react.js
+++ public/js/home/react.js
@@ -437,8 +437,8 @@ class Card extends React.Component {
                 return(
                     <a
                         key={`${type}-${index}`}
-                        href={anime.type === 'manga' ? anime.url : null}
-                        onClick={anime.type === 'anime' ? () => props.changeModal(anime.url) : null}
+                        href={anime.type === 'manga' ? anime.url : null}                                // If paper medium, redirect
+                        onClick={anime.type === 'anime' ? () => props.changeModal(anime.url) : null}    // If anime, open new modal
                         className="related-cont">
                         <div className={`related-anime ${anime.type}`}>
                             <div className="related-anime-title">
@@ -460,7 +460,6 @@ class Card extends React.Component {
             )
         }
 
-
         return(
             <div className="content content-related">
                 {relationships}
@@ -468,11 +467,12 @@ class Card extends React.Component {
         )
     }
 
-
+    // Episodes; has link to video, discussion forum, episode list, etc.
     function Episodes(props){
         if(!props.episodes || !props.episodes.length){
             return (<p>No episode data available </p>)
         }
+
         const episodes = props.episodes.map((ep) => {
             return(
                 <tr key={ep.id} className="ep-row">
@@ -513,198 +513,210 @@ class Card extends React.Component {
             )
         }
 
+// ================================================================
+// ================================================================
+// MAIN COMPONENT: Sidebar
+// ================================================================
+// ================================================================
 
+    class Sidebar extends React.Component {
+        constructor(props){
+            super(props);
 
+            this.genres = [
+                "Action", "Adventure", "Cars", "Comedy", "Dementia",
+                "Demons", "Mystery", "Drama", "Ecchi", "Fantasy", "Game",
+                "Historical", "Horror", "Magic", "Martial Arts",
+                "Mecha", "Music", "Parody", "Samurai", "Romance",
+                "School", "Sci-Fi", "Shoujo", "Shoujo Ai", "Shounen",
+                "Shounen Ai", "Space", "Sports", "Super Power", "Vampire",
+                "Harem", "Slice of Life", "Supernatural", "Military", "Police",
+                "Psychological", "Thriller", "Seinen", "Josei"
+            ]
 
+            this.state = {
+                tab: 'search', // default to this
+                visible: false
+            }
+        }
 
-        // ================================================================
+        componentDidMount(){
+            // Click anywhere else to close sidebar
+            document.addEventListener("click", (ev) => {
+                if(!ev.path.includes(document.querySelector('.sidebar-cont'))){
+                    this.hideSidebar()
+                }
+            });
+        }
 
-        class Sidebar extends React.Component {
-            constructor(props){
-                super(props);
+        showSidebar(tab){
+            this.setState({visible: true, tab: tab})
+        }
 
-                this.genres = [
-                    "Action", "Adventure", "Cars", "Comedy", "Dementia",
-                    "Demons", "Mystery", "Drama", "Ecchi", "Fantasy", "Game",
-                    "Historical", "Horror", "Magic", "Martial Arts",
-                    "Mecha", "Music", "Parody", "Samurai", "Romance",
-                    "School", "Sci-Fi", "Shoujo", "Shoujo Ai", "Shounen",
-                    "Shounen Ai", "Space", "Sports", "Super Power", "Vampire",
-                    "Harem", "Slice of Life", "Supernatural", "Military", "Police",
-                    "Psychological", "Thriller", "Seinen", "Josei"
-                ]
+        hideSidebar(){
+            this.setState({visible: false})
+        }
 
-                this.state = {
-                    tab: 'search', // default to this
-                    visible: false
+        changeFilter(data, type){
+            let search = this.props.filter;
+            search[type] = data.target.value;
+
+            this.props.handleFilter(search);
+        }
+
+        changeGenres(data, type){
+            // On clear, make it empty
+            if(type === 'clear'){
+                return this.props.handleGenre([])
+            }
+
+            let genresCopy = this.props.genres;
+
+            // On add, add in the select box's data
+            if(type === 'add'){
+                genresCopy.includes(data) ? null : genresCopy.push(data);
+            }
+            // Else, find index and remove
+            else if(type === 'remove'){
+                if(genresCopy.includes(data)){
+                    let index = genresCopy.indexOf(data);
+                    genresCopy.splice(index, 1);
                 }
             }
 
-            componentDidMount(){
-                document.addEventListener("click", (ev) => {
-                    if(!ev.path.includes(document.querySelector('.sidebar-cont'))){
-                        this.hideSidebar()
-                    }
-                });
-            }
+            // Send back to update current filter
+            this.props.handleGenre(genresCopy);
+        }
 
-            showSidebar(tab){
-                this.setState({visible: true, tab: tab})
-            }
+        removeFavorites(data){
+            let favoritesCopy = this.props.favorites;
+            let index = -1;
 
-            hideSidebar(){
-                this.setState({visible: false})
-            }
-
-            changeFilter(data, type){
-                let search = this.props.filter;
-                search[type] = data.target.value;
-
-                this.props.handleFilter(search);
-            }
-
-            changeGenres(data, type){
-                if(type === 'clear'){
-                    return this.props.handleGenre([])
+            favoritesCopy.forEach((item) => {
+                if(data.title === item.title){
+                    index = favoritesCopy.indexOf(item);
+                    favoritesCopy.splice(index, 1);
                 }
+            })
 
-                let genresCopy = this.props.genres;
-                // We're actually passing in the select box's data
+            return this.props.handleFavorites(favoritesCopy)
+        }
 
-                if(type === 'add'){
-                    genresCopy.includes(data) ? null : genresCopy.push(data);
-                }
-                else if(type === 'remove'){
-                    if(genresCopy.includes(data)){
-                        let index = genresCopy.indexOf(data);
-                        genresCopy.splice(index, 1);
-                    }
-                }
+        clearFavorites(){
+            return this.props.handleFavorites([])
+        }
 
-                this.props.handleGenre(genresCopy);
-            }
-
-            removeFavorites(data){
-                let favoritesCopy = this.props.favorites;
-                let index = -1;
-
-                favoritesCopy.forEach((item) => {
-                    if(data.title === item.title){
-                        index = favoritesCopy.indexOf(item);
-                        favoritesCopy.splice(index, 1);
-                    }
-                })
-
-                return this.props.handleFavorites(favoritesCopy)
-            }
-
-            clearFavorites(){
-                return this.props.handleFavorites([])
-            }
-
-
-            render(){
-                let currentTab;
-                return(
-                    <div className={`sidebar-cont ${this.state.visible ? 'show' : 'hide'}`}>
-                        <div className={`sidebar-content ${this.state.tab}`}>
-                            <Search
-                                changeFilter={(data, type) => this.changeFilter(data, type)}
+        render(){
+            let currentTab;
+            return(
+                <div className={`sidebar-cont ${this.state.visible ? 'show' : 'hide'}`}>
+                    <div className={`sidebar-content ${this.state.tab}`}>
+                        <Search
+                            changeFilter={(data, type) => this.changeFilter(data, type)}
+                        />
+                        <Genres
+                            allGenres={this.genres}
+                            currentGenres={this.props.genres}
+                            changeGenres={(data, type) => this.changeGenres(data, type)}/>
+                            <Favorites
+                                removeFavorites={(data) => this.removeFavorites(data)}
+                                favorites={this.props.favorites}
+                                clearFavorites={() => this.clearFavorites()}
+                                favoritesOnly={() => this.props.favoritesOnly()}
                             />
-                            <Genres
-                                allGenres={this.genres}
-                                currentGenres={this.props.genres}
-                                changeGenres={(data, type) => this.changeGenres(data, type)}/>
-                                <Favorites
-                                    removeFavorites={(data) => this.removeFavorites(data)}
-                                    favorites={this.props.favorites}
-                                    clearFavorites={() => this.clearFavorites()}
-                                    favoritesOnly={() => this.props.favoritesOnly()}
-                                />
-                            </div>
-
-                            <div className="sidebar-btns">
-                                <div className="sidebar-search" onClick={() => this.showSidebar('search')}>Search</div>
-                                <div className="sidebar-genres" onClick={() => this.showSidebar('genre')}>Genres</div>
-                                <div className="sidebar-favorites" onClick={() => this.showSidebar('favorite')}>Favorites</div>
-                            </div>
                         </div>
-                    )
-                }
 
-            }
-
-
-            function Search(props){
-                return (
-                    <div className="search-cont side-content">
-                        <h1>Search</h1>
-                        <label>Title</label>
-                        <input className="search-name" onChange={(ev) => props.changeFilter(ev, 'title')}></input>
-                        <label>Studio</label>
-                        <input className="search-name" onChange={(ev) => props.changeFilter(ev, 'studio')}></input>
-                        <label>Content</label>
-                        <input className="search-name" onChange={(ev) => props.changeFilter(ev, 'synopsis')}></input>
+                        <div className="sidebar-btns">
+                            <div className="sidebar-search" onClick={() => this.showSidebar('search')}>Search</div>
+                            <div className="sidebar-genres" onClick={() => this.showSidebar('genre')}>Genres</div>
+                            <div className="sidebar-favorites" onClick={() => this.showSidebar('favorite')}>Favorites</div>
+                        </div>
                     </div>
                 )
             }
 
-            function Genres(props){
-                const allGenres = props.allGenres.map((genre) => {
-                    return (<option key={genre} value={genre}>{genre}</option>)
-                });
+        }
 
-                const ele = document.querySelector('.genre-select');
+        // * * * * * * * * * * * * * * * * * * * *
+        // SUBCOMPONENT: Each sidetab
+        // * * * * * * * * * * * * * * * * * * * *
 
-                const allCurrentGenres = props.currentGenres
-                ?   props.currentGenres.map((genre) => {
+        // Searching via text
+        function Search(props){
+            return (
+                <div className="search-cont side-content">
+                    <h1>Search</h1>
+                    <form>
+                        <label htmlFor="search-name">Title</label>
+                            <input id="search-name" onChange={(ev) => props.changeFilter(ev, 'title')}></input>
+                        <label htmlFor="search-studio">Studio</label>
+                            <input id="search-studio" onChange={(ev) => props.changeFilter(ev, 'studio')}></input>
+                        <label htmlFor="search-content">Content</label>
+                            <input id="search-content" onChange={(ev) => props.changeFilter(ev, 'synopsis')}></input>
+                    </form>
+                </div>
+            )
+        }
+
+        // Filter via genre
+        function Genres(props){
+            const allGenres = props.allGenres.map((genre) => {
+                return (<option key={genre} value={genre}>{genre}</option>)
+            });
+
+            const ele = document.querySelector('.genre-select');
+
+            const allCurrentGenres = props.currentGenres
+            ?   props.currentGenres.map((genre) => {
                     return (<span className="genre-on" onClick={() => props.changeGenres(genre, 'remove')}>{genre}</span>)
                 })
-                :   null;
+            :   null;
 
-                return (
-                    <div className="genre-cont side-content">
-                        <h1>Genre</h1>
-                        <select
-                            onDoubleClick={() => props.changeGenres(ele.options[ele.selectedIndex].value, 'add')}
-                            className="genre-select" size='6'>
-                            {allGenres}
-                        </select>
-                        <div className="genre-btns">
-                            <div>
-                                <button onClick={() => props.changeGenres(ele.options[ele.selectedIndex].value, 'add')}>Add</button>
-                                <button onClick={() => props.changeGenres(ele.options[ele.selectedIndex].value, 'remove')}>Remove</button>
-                            </div>
-                            <button onClick={() => props.changeGenres(null, 'clear')}>Clear</button>
-
+            return (
+                <div className="genre-cont side-content">
+                    <h1>Genre</h1>
+                    <select
+                        onDoubleClick={() => props.changeGenres(ele.options[ele.selectedIndex].value, 'add')}
+                        className="genre-select" size='6'>
+                        {allGenres}
+                    </select>
+                    <div className="genre-btns">
+                        <div>
+                            <button onClick={() => props.changeGenres(ele.options[ele.selectedIndex].value, 'add')}>Add</button>
+                            <button onClick={() => props.changeGenres(ele.options[ele.selectedIndex].value, 'remove')}>Remove</button>
                         </div>
-                        {allCurrentGenres}
+                        <button onClick={() => props.changeGenres(null, 'clear')}>Clear</button>
+
+                    </div>
+                    {allCurrentGenres}
+                </div>
+            )
+        }
+
+        // Filter via favorites
+        function Favorites(props){
+            let favorites = props.favorites.map((favorite) => {
+                return(
+                    <div key={favorite.title} className="favorite-show" onClick={() => props.removeFavorites(favorite)}>
+                        {favorite.title}
                     </div>
                 )
+            })
+            return (
+                <div className="favorite-cont side-content">
+                    <h1>Favorites</h1>
+                    <div className="favorite-btns">
+                        <button onClick={() => props.favoritesOnly()}>Toggle Favorites</button>
+                        <button onClick={() => props.clearFavorites()}>Clear All</button>
+                    </div>
+
+                    {favorites}
+                </div>)
             }
-            function Favorites(props){
-                let favorites = props.favorites.map((favorite) => {
-                    return(
-                        <div key={favorite.title} className="favorite-show" onClick={() => props.removeFavorites(favorite)}>
-                            {favorite.title}
-                        </div>
-                    )
-                })
-                return (
-                    <div className="favorite-cont side-content">
-                        <h1>Favorites</h1>
-                        <div className="favorite-btns">
-                            <button onClick={() => props.favoritesOnly()}>Toggle Favorites</button>
-                            <button onClick={() => props.clearFavorites()}>Clear All</button>
-                        </div>
 
-                        {favorites}
-                    </div>)
-                }
-
-                // ================================================================
-                // ================================================================
-                // ================================================================
+            // ================================================================
+            // ================================================================
+            // ================================================================
 
                 class Season extends React.Component{
                     constructor(props){
