dd15d94eece86f16c35c691f9910c9710ac2c7b6
pomfy113 <pomfy113@gmail.com>
2018-03-27 13:36:54 -0700

    Starting rework

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ app.set('view engine', 'handlebars');
 // Anime routes
 require('./controllers/anime.js')(app);
 
-
 // double: either port for heroku or local 3000
 app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
