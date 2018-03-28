232876a7f8e107a7fad20d2ed5ce3adde1cc0aba
pomfy113 <pomfy113@gmail.com>
2018-03-27 19:46:57 -0700

    Running smoothly; everything checks out

--- oac-front/src/index.js
+++ oac-front/src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import './css/home.css'
+import './css/modal.css'
+
 
 ReactDOM.render(<App/>, document.getElementById('root'));
 registerServiceWorker();
